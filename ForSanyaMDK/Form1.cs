3c071d291689a7febbd41ce340180356a1093c8c
Александр Миначенко <you@example.com>
2023-09-14 04:39:01 +0400

    first commit

--- ForSanyaMDK/Form1.cs
+++ ForSanyaMDK/Form1.cs
@@ -92,7 +92,7 @@ namespace ForSanyaMDK
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int rowCountToLoad = 4; // количество строк
+            int rowCountToLoad = 5; // количество строк
             LoadDataFromSQLite(rowCountToLoad);
 
             //  Высота строк в DataGridView.
