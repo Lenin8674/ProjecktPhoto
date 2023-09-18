791156bd4ad4a971b771c6c7637774e4c294c612
Александр Миначенко <you@example.com>
2023-09-18 14:44:18 +0400

    first commit

---
+++ ForSanyaMDK/Form2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ForSanyaMDK
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "admin" && textBox2.Text == "1")
+            {
+                MessageBox.Show("Добро пожаловать admin", "Успешно");
+                Form3 form = new Form3();
+                form.Show();
+                this.Hide();
+             
+            }
+            else if (textBox1.Text == "user" && textBox2.Text == "2")
+            {
+                MessageBox.Show("Добро пожаловать user", "Успешно");
+                Form1 form = new Form1();
+                form.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Вы ввели неправильный логин или пароль", "Ошибка авторизации");
+            }
+        }
+    }
+}
---
+++ ForSanyaMDK/Form3.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using System.IO;
+
+namespace ForSanyaMDK
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+            dataGridView1.AllowUserToAddRows = false;
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            int rowCountToLoad = 5; // количество строк
+            LoadDataFromSQLite(rowCountToLoad);
+
+            //  Высота строк в DataGridView.
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                row.Height = 385; // высота строк в пикснлях.
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        private void LoadDataFromSQLite(int rowCount)
+        {
+            string connectionString = "Data Source=your_database.db;Version=3;";
+
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+
+                string sqlQuery = $"SELECT * FROM ImagesTable LIMIT {rowCount};";
+
+                using (SQLiteCommand cmd = new SQLiteCommand(sqlQuery, connection))
+                {
+                    using (SQLiteDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            dataGridView1.ColumnCount = reader.FieldCount;
+
+                            for (int i = 0; i < reader.FieldCount; i++)
+                            {
+                                dataGridView1.Columns[i].Name = reader.GetName(i);
+
+                                if (reader.GetFieldType(i) == typeof(byte[]))
+                                {
+                                    DataGridViewImageColumn imageColumn = new DataGridViewImageColumn();
+                                    imageColumn.HeaderText = reader.GetName(i);
+                                    dataGridView1.Columns.RemoveAt(i);
+                                    dataGridView1.Columns.Insert(i, imageColumn);
+                                }
+
+                                dataGridView1.Columns[i].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+                            }
+                        }
+
+                        while (reader.Read())
+                        {
+                            DataGridViewRow row = new DataGridViewRow();
+                            for (int i = 0; i < reader.FieldCount; i++)
+                            {
+                                if (reader.GetFieldType(i) == typeof(byte[]))
+                                {
+                                    byte[] imageData = (byte[])reader[i];
+                                    if (imageData != null && imageData.Length > 0)
+                                    {
+                                        using (MemoryStream ms = new MemoryStream(imageData))
+                                        {
+                                            Image image = Image.FromStream(ms);
+                                            row.Cells.Add(new DataGridViewImageCell
+                                            {
+                                                Value = image
+                                            });
+                                        }
+                                    }
+                                    else
+                                    {
+                                        row.Cells.Add(new DataGridViewImageCell());
+                                    }
+                                }
+                                else
+                                {
+                                    row.Cells.Add(new DataGridViewTextBoxCell
+                                    {
+                                        Value = reader[i]
+                                    });
+                                }
+                            }
+
+                            dataGridView1.Rows.Add(row);
+                        }
+                    }
+                }
+
+                connection.Close();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 form = new Form2();
+            form.Show();
+            this.Hide();
+        }
+    }
+}
+
